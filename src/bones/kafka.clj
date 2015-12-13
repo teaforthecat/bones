844621a6817cff0877e0b328e8f3dbe72e2ae9b6
Chris Thompson <chris.thompson@govdelivery.com>
2015-12-12 21:26:20 -0600

    kafka produce/consume with a test

--- src/bones/kafka.clj
+++ src/bones/kafka.clj
@@ -1,11 +1,14 @@
 (ns bones.kafka
   (:require [bones.serializer :refer [serialize deserialize]]
+            [clj-kafka.core :refer [with-resource]]
+            [clj-kafka.zk :as zk]
+            [clj-kafka.consumer.zk :as zkc]
             [clj-kafka.new.producer :as nkp]))
 
 
 ; TODO move producer to system so we don't call .close on it everytime
 ; returns a future
-(defn send [topic data]
+(defn produce [topic data]
   (let [bytes (serialize data)
         record (nkp/record topic bytes)
         producer-config {"bootstrap.servers" "127.0.0.1:9092"}]
@@ -13,3 +16,13 @@
                                 (nkp/byte-array-serializer)
                                 (nkp/byte-array-serializer))]
       (nkp/send p record))))
+
+
+(defn consume [topic]
+  ;; consumer must be at position 0 in bindings
+  ;; if we crash, group.id must change or reboot service (?)
+  (with-resource [c (zkc/consumer {"zookeeper.connect" "127.0.0.1:2181"
+                                   "group.id" "bones.kafka1"
+                                   "auto.offset.reset" "smallest"})]
+    zkc/shutdown
+    (update (first (zkc/messages c topic)) :value deserialize)))
--- test/bones/kafka_test.clj
+++ test/bones/kafka_test.clj
@@ -1,59 +1,19 @@
 (ns bones.kafka-test
-  (:require [clojure.test.check :as tc]
+  (:require [clojure.test :refer [deftest testing is use-fixtures]]
+            [bones.kafka :as kafka]
+            [clojure.test.check :as tc]
             [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :as prop]
             [clj-kafka.zk :as zk]
             [clj-kafka.new.producer :as kp]
             [bones.serializer :refer [serialize deserialize]]))
 
-(def run-count 1)
-(def zookeeper-connect "127.0.0.1:2181")
-(def broker-list
-    (zk/broker-list
-     (zk/brokers {"zookeeper.connect" zookeeper-connect})))
-(def producer-config {"bootstrap.servers" broker-list})
-(def producer (kp/producer producer-config
-                           (kp/byte-array-serializer)
-                           (kp/byte-array-serializer)))
 
-(defn consumer-first-config [] {"zookeeper.connect" zookeeper-connect
-                                "group.id" (str (java.util.UUID/randomUUID))
-                                "auto.offset.reset" "smallest"
-                                "auto.commit.enable" "false"})
-
-(defn fetch-first [topic]
-  (with-resource [c (zk/consumer (consumer-first-config))]
-    zk/shutdown
-    (first (zk/messages c topic))))
-
-(defn handle-complex-command [segment]
-  segment)
-
-(defn handle-simple-command [segment]
-  segment)
-
-(def background-jobs
-  [::handle-complex-command
-   ::handle-simple-command])
-
-(def background-onyx-config {})
-
-(defn setup []
-  (submit-jobs background-onyx-config
-               (build-jobs background-onyx-config background-jobs)))
-
-(defn send-to-background [segment]
-  @(kp/send producer (kp/record ::handle-simple-command-input (serialize segment))))
-
-;; TODO: namespaced symbol to string mapping and (string to namespaced symbol)
-(defn read-from-background []
-  (fetch-first ::handle-simple-command-input))
-
-
-;; @WIP
-(def input-equals-output
-  (prop/for-all [v (gen/vector gen/int)]
-                (let [segment v
-                      submission (send-to-background (first background-jobs) v)]
-                  (= segment
-                     (read-from-background (first background-jobs))))))
+(deftest round-trip
+  (testing "a simple string"
+    ;; the consumer's offset is important here
+    ;; TODO somehow get the right offset for this message
+    (let [send-response @(kafka/produce "input" "simple-string" )
+          consume-response (kafka/consume "input")]
+      (is (= "simple-string" (:value consume-response)))
+      )))
